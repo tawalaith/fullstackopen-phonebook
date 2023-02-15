7e568787fa5130dc89f6fedd724aa69ad5512ad4
Laith Tawabini <75672689+tawalaith@users.noreply.github.com>
2023-02-15 12:42:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 //order of middlewares matters
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
